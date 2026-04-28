8e451424fcb1c08aa66a0bf6c1d12dde1ed8b459
vicso-name <vic.sokoliuk@gmail.com>
2026-04-28 15:34:38 +0300

    feat: add Google Analytics tracking

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { Manrope, Roboto, Roboto_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -43,6 +44,19 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={`${manrope.variable} ${roboto.variable} ${robotoMono.variable} font-sans`}
       >
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-1PZTFJRNGL"
+            strategy="afterInteractive"
+          />
+
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1PZTFJRNGL');
+            `}
+          </Script>
         {children}
       </body>
     </html>
